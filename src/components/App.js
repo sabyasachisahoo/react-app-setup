edf8f370b06391c3b5e7bb0a2e98537c74e99c4a
sabyasachisahoo <sabyasachisahoo77@gmail.com>
2019-12-13 01:26:40 +0530

    added subject with multicast

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,33 @@
 import React , { Component } from 'react';
 import '../styles/App.scss';
 import fetchUsers$ from '../services/data.service';
+import { Subject } from 'rxjs';
+import { multicast } from 'rxjs/operators';
 
 class App extends Component {
 
     constructor(){
         super();
         this.state = {
-            users:[]
+            users:[],
+            email: []
         }
     }
     
     componentDidMount() {
-        fetchUsers$.subscribe(res => {
-            this.setState({ users : res.data}) 
+        const subject = new Subject();
+        const multicasted = fetchUsers$.pipe(multicast(subject));
+        multicasted.subscribe({
+            next: (v) => this.setState({users : v.data})
         });
+
+        multicasted.subscribe({
+            next: (v) => this.setState({email : v.data[0].email})
+          });
+
+        
+        
+        multicasted.connect();
 
     }
     render() {
@@ -23,6 +36,9 @@ class App extends Component {
                  <h1>Hello World :) </h1>
                  {
                      this.state.users.map(user => <h1 key={user.id}>{user.name}</h1>)
+                 }
+                 {
+                     <h1>Observer 2 {this.state.email}</h1>
                  }
             </div>
           
